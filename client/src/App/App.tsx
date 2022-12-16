cb57d82406dfb70d667335a2f7e79a3b367fb015
Mohamed Bakoush <bakoushmohamed@gmail.com>
2022-12-16 16:55:03 +0000

    App File

---
+++ client/src/App/App.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+function App() {  
+  useEffect(() => {
+      document.title = "Page not found";
+  });
+  return ( 
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default App;
