5d3b56ac9ce0c209131b3b9a32b0688032dc74a7
Mohamed Bakoush <bakoushmohamed@gmail.com>
2022-12-22 03:29:44 +0000

    remove log confirmation

--- backend/database/dbSetup.js
+++ backend/database/dbSetup.js
@@ -4,8 +4,6 @@ const FileSystem = require("fs");
 const sql = FileSystem.readFileSync(__dirname + "/init.sql").toString();
  
 client.connect()
-.then(() => console.log("Connected successfuly")) 
 .then(() => client.query(sql))
-.then(() => console.log("Table phrases Created")) 
 .catch(e => console.log(e))
 .finally(() => client.end());
