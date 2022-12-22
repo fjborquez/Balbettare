62bd94e75089773147ca049be55de1c4123f2113
Mohamed Bakoush <bakoushmohamed@gmail.com>
2022-12-22 03:29:19 +0000

    INSERT DATA INTO phrases

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -3,3 +3,6 @@ CREATE TABLE phrases (
     english VARCHAR(200) NOT NULL, 
     italian VARCHAR(200) NOT NULL
 );
+
+INSERT INTO phrases (english, italian) VALUES ('Good evening, Mrs. Marchi!', 'Buonasera, signora Marchi!');
+INSERT INTO phrases (english, italian) VALUES ('Pleased to meet you!', 'Piacere!');
