e9b0a1498dad270caa9633feaf514a8c5a622f0b
Mohamed Bakoush <bakoushmohamed@gmail.com>
2023-02-04 22:47:01 +0000

    backend -> database

---
+++ database/server.js
@@ -0,0 +1,37 @@
+"use strict";
+const FileSystem = require("fs");
+const express = require("express"); 
+const { client } = require("./postgresql/dbConfig");
+require("dotenv").config();
+const app = express(); 
+ 
+const data = {
+    phrases: undefined
+}
+
+async function getPhrases() { 
+    client.connect();
+    const phrases = await client.query("SELECT * FROM phrases")
+    client.end();
+    return data.phrases = phrases.rows; 
+}
+
+app.get("/say-phrase", sayPhrase);
+async function sayPhrase(req, res) {
+    if (data.phrases === undefined) {await getPhrases()}; 
+    const random = Math.floor(Math.random() * data.phrases.length);  
+    const phrase = data.phrases[random];
+    res.json({
+        phrase: phrase.italian,
+        translatedPhrase: phrase.english
+    });  
+}
+
+app.get("*", function(req, res){
+    res.status(404).redirect("/");
+});  
+
+app.set("port", (process.env.DATABASE_PORT || 5000));
+app.listen(app.get("port"), function() { 
+  console.log("Server running at:" + app.get("port")); 
+});
