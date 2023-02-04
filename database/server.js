90cd9701efb17a46c848d3866277fc6562c4f268
Mohamed Bakoush <bakoushmohamed@gmail.com>
2023-02-04 23:11:18 +0000

    Docker: database

--- database/server.js
+++ database/server.js
@@ -31,7 +31,7 @@ app.get("*", function(req, res){
     res.status(404).redirect("/");
 });  
 
-app.set("port", (process.env.DATABASE_PORT || 5000));
+app.set("port", (process.env.DATABASE_PORT || 4000));
 app.listen(app.get("port"), function() { 
   console.log("Server running at:" + app.get("port")); 
 });
