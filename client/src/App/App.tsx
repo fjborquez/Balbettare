6fd407dbad1b0ce3603e33c22d8c5f38e9894ba1
Mohamed Bakoush <bakoushmohamed@gmail.com>
2023-02-13 19:34:52 +0000

    phrase similarity
    if > 80% allow user to advance to the next phase

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -142,6 +142,53 @@ export default function App() {
     });
   }  
 
+  function clean_phrase(input_phrase: string , output_phrase: string){ 
+    const cleaned_output_phrase = output_phrase.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s{2,}/g, " ").replace(/\s+$/g, '').replace(/^\s+|\s+$/g, '');
+    const cleaned_input_phrase = input_phrase.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s{2,}/g, " ").replace(/\s+$/g, '').replace(/^\s+|\s+$/g, '');
+    return similarity(cleaned_output_phrase, cleaned_input_phrase) >= 0.8;
+  }
+
+  function similarity(s1: string | any[], s2: string | any[]) {
+    var longer = s1;
+    var shorter = s2;
+    if (s1.length < s2.length) {
+      longer = s2;
+      shorter = s1;
+    }
+    var longerLength = longer.length;
+    if (longerLength == 0) {
+      return 1.0;
+    } 
+    return(longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+  }
+
+  function editDistance(s1: string, s2: string) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+  
+    var costs = new Array();
+    for (var i = 0; i <= s1.length; i++) {
+      var lastValue = i;
+      for (var j = 0; j <= s2.length; j++) {
+        if (i == 0)
+          costs[j] = j;
+        else {
+          if (j > 0) {
+            var newValue = costs[j - 1];
+            if (s1.charAt(i - 1) != s2.charAt(j - 1))
+              newValue = Math.min(Math.min(newValue, lastValue),
+                costs[j]) + 1;
+            costs[j - 1] = lastValue;
+            lastValue = newValue;
+          }
+        }
+      }
+      if (i > 0)
+        costs[s2.length] = lastValue;
+    }
+    return costs[s2.length];
+  }
+
   return (
     <article>  
       {stream.access ? (
@@ -160,7 +207,7 @@ export default function App() {
             }
 
             <p>Phrase: {phrase.sayPhrase}</p>
-
+ 
             {
               (transcription.available && !transcription.analyzing)
               &&  
@@ -168,11 +215,13 @@ export default function App() {
                 <p>
                   Transcript: {transcription.data}
                 </p>
-                <input 
-                  type={"button"}
-                  onClick={updateSayPhrase} 
-                  value="Next"
-                />
+                {clean_phrase(phrase.sayPhrase, transcription.data) ? 
+                  <input 
+                    type={"button"}
+                    onClick={updateSayPhrase} 
+                    value="Next"
+                  />
+                : undefined}
               </section>
             }  
           </section>  
