c8719b308a21eb1682e50619808000e894dd084b
Mohamed Bakoush <bakoushmohamed@gmail.com>
2023-04-10 13:37:45 +0100

    phrase counter rather then random

--- database/server.js
+++ database/server.js
@@ -9,24 +9,46 @@ require("dotenv").config();
 const app = express(); 
 app.use(cors())
 
+// Cache for Italian_Phrases
 let Italian_Phrases = undefined;
+
+// Counter for cycling through Italian_Phrases
+let Italian_Phrase_Counter = 0;
 
 app.get("/italianPhrase", italianPhrase);
 async function italianPhrase(req, res) {  
-    if (Italian_Phrases == undefined) {
-        client.connect();
-        const data = await client.query("SELECT * FROM Italian_Phrases")
-        client.end();
-        Italian_Phrases = data.rows;
-    }  
+    try {
+        // Check if Italian_Phrases is cached
+        if (Italian_Phrases == undefined) {
+            // Connect to the database
+            client.connect();
+            // Query the database for all Italian phrases
+            const data = await client.query("SELECT * FROM Italian_Phrases");
+            // Cache the results in the Italian_Phrases variable
+            Italian_Phrases = data.rows;
+            // Disconnect from the database
+            await client.end();
+        }  
+    
+        // Get a random phrase from Italian_Phrases 
+        const phrase = Italian_Phrases[Italian_Phrase_Counter];
+        
+        // Increment the counter and reset it if it exceeds the length of Italian_Phrases
+        Italian_Phrase_Counter = (Italian_Phrase_Counter + 1) % Italian_Phrases.length;
 
-    const random = Math.floor(Math.random() * Italian_Phrases.length);  
-    const phrase = Italian_Phrases[random];
+        // Return the selected phrase and its translation
+        res.json({
+            phrase: phrase.italian_phrase,
+            translatedPhrase: phrase.english_translation
+        });   
+    } catch (error) {
+        console.error("Error fetching Italian phrase:", error);
 
-    res.json({
-        phrase: phrase.italian_phrase,
-        translatedPhrase: phrase.english_translation
-    });  
+        // Return an error response with a status code
+        res.status(500).json({
+            error: "An error occurred while fetching an Italian phrase. Please try again later."
+        });
+    }
 }
 
 app.get("*", function(req, res){
