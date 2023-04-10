5f78d0a4a607e56f5923f514da256125cd25d6d1
Mohamed Bakoush <bakoushmohamed@gmail.com>
2023-04-10 13:21:55 +0100

    Additional Italian phrases

--- database/postgresql/init.sql
+++ database/postgresql/init.sql
@@ -1,3 +1,4 @@
+-- Create the Italian_Phrases table
 CREATE TABLE Italian_Phrases (
     Phrase_id BIGSERIAL NOT NULL, 
     Italian_Phrase VARCHAR(255) NOT NULL, 
@@ -5,5 +6,12 @@ CREATE TABLE Italian_Phrases (
     PRIMARY KEY (Phrase_id)
 );
 
-INSERT INTO Italian_Phrases (Italian_Phrase, English_Translation) VALUES ('Buonasera, signora Marchi!', 'Good evening, Mrs. Marchi!');
-INSERT INTO Italian_Phrases (Italian_Phrase, English_Translation) VALUES ('Piacere!', 'Pleased to meet you!');
+-- Insert basic Italian phrases with their English translations
+INSERT INTO Italian_Phrases (Italian_Phrase, English_Translation) 
+VALUES ('Buonasera, signora Marchi!', 'Good evening, Mrs. Marchi!'),
+       ('Piacere!', 'Pleased to meet you!'),
+       ('Ciao!', 'Hello!'),
+       ('Buongiorno!', 'Good morning!'),
+       ('Buon pomeriggio!', 'Good afternoon!'),
+       ('Buona notte!', 'Good night!'),
+       ('Come stai?', 'How are you?');
