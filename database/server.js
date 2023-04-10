5de64aea59af443cbbb6d020da8a017e829393d2
Mohamed Bakoush <bakoushmohamed@gmail.com>
2023-04-10 14:01:49 +0100

    italianPhrase  -> languagePhrase

--- database/server.js
+++ database/server.js
@@ -9,44 +9,53 @@ require("dotenv").config();
 const app = express(); 
 app.use(cors())
 
-// Cache for Italian_Phrases
-let Italian_Phrases = undefined;
+// Cache for Phrases and counters
+let LanguageData = {};
 
-// Counter for cycling through Italian_Phrases
-let Italian_Phrase_Counter = 0;
+async function fetchPhrase(language) {
+    // Connect to the database
+    client.connect();
+    // Query the database for all Italian phrases
+    const data = await client.query(`SELECT * FROM ${language}_Phrases`);
+    // Cache the results in the LanguageData variable
+    LanguageData[language] = {
+        phrases: data.rows,
+        counter: 0
+    };
+    // Disconnect from the database
+    await client.end();
+}
 
-app.get("/italianPhrase", italianPhrase);
-async function italianPhrase(req, res) {  
+app.get("/italianPhrase", languagePhrase);
+
+// Fetch a phrase from the database (Currently only supports Italian)
+async function languagePhrase(req, res) {  
     try {
-        // Check if Italian_Phrases is cached
-        if (Italian_Phrases == undefined) {
-            // Connect to the database
-            client.connect();
-            // Query the database for all Italian phrases
-            const data = await client.query("SELECT * FROM Italian_Phrases");
-            // Cache the results in the Italian_Phrases variable
-            Italian_Phrases = data.rows;
-            // Disconnect from the database
-            await client.end();
-        }  
-    
-        // Get a random phrase from Italian_Phrases 
-        const phrase = Italian_Phrases[Italian_Phrase_Counter];
-        
-        // Increment the counter and reset it if it exceeds the length of Italian_Phrases
-        Italian_Phrase_Counter = (Italian_Phrase_Counter + 1) % Italian_Phrases.length;
+        // Get the language from the query string
+        const language = req.query.language || "Italian";
+
+        // Check if the phrases for the specified language are cached
+        if (LanguageData[language] === undefined) {
+            await fetchPhrase(language);
+        }
+         
+        // Get a phrase from the cached phrases for the specified language
+        const phrase = LanguageData[language].phrases[LanguageData[language].counter];
+
+        // Increment the counter and reset it if it exceeds the length of the phrases for the specified language
+        LanguageData[language].counter = (LanguageData[language].counter + 1) % LanguageData[language].phrases.length;
 
         // Return the selected phrase and its translation
         res.json({
-            phrase: phrase.italian_phrase,
-            translatedPhrase: phrase.english_translation
+            phrase: phrase[language.toLowerCase() + "_phrase"],
+            translatedPhrase: phrase["english_translation"]
         });   
     } catch (error) {
-        console.error("Error fetching Italian phrase:", error);
+        console.error(`Error fetching ${language} phrase:`, error);    
 
         // Return an error response with a status code
         res.status(500).json({
-            error: "An error occurred while fetching an Italian phrase. Please try again later."
+            error: `An error occurred while fetching a ${language} phrase. Please try again later.`
         });
     }
 }
